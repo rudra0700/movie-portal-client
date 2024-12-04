439d30abf7089d0ad7b26c000af358160b1c9671
rudra0700 <baruarudra7@gmail.com>
2024-12-04 13:28:30 +0600

    feat:login functionality added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,14 +16,14 @@ const Login = () => {
         const password = e.target.password.value;
           
         loginUser(email, password)
-        .then(res =>{
+        .then(res => {
+            console.log(res);
             const user = res.user;
             setUser(user);
-            console.log(user);
             navigate(location?.state ? location?.state.from : '/')
         })
         .catch(error => {
-            setError(error.message);
+            console.log(error.message);
         })
         
     }
@@ -37,8 +37,8 @@ const Login = () => {
     return (
         <div className='flex justify-center items-center'>
              <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
-        <div className="form-control" onSubmit={handleSubmit}>
+      <form className="card-body"  onSubmit={handleSubmit}>
+        <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
