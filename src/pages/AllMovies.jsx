e0c6f4b8ed921dcc74d3c623dbc56dea6603cea8
rudra0700 <baruarudra7@gmail.com>
2024-12-04 20:06:44 +0600

    all movies pages updated

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import FeaturedMovies from '../components/FeaturedMovies';
 
 const AllMovies = () => {
+    const data = useLoaderData();
+     const [allMovies, setAllMovies] = useState(data);
+
     return (
         <div>
-            from all movie page
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    allMovies.map(movie => <FeaturedMovies key={movie._id} movie={movie}></FeaturedMovies>)
+                }
+            </div>
         </div>
     );
 };
--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -30,7 +30,8 @@ const router = createBrowserRouter([
         },
         {
           path: '/allMovies',
-          element: <AllMovies></AllMovies>
+          element: <AllMovies></AllMovies>,
+          loader : () => fetch("http://localhost:5000/movies")
         }
       ]
     },
