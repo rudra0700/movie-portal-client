a00e1ccadf88bcc4b09ef1519081a3ebdf7a73d6
rudra0700 <baruarudra7@gmail.com>
2024-12-04 10:45:32 +0600

    navbar added

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const MainLayout = () => {
+    return (
+        <div>
+             <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainLayout;
