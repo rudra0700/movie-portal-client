e25bf8f752061cdf545f1b68d4d41941ce516f55
rudra0700 <baruarudra7@gmail.com>
2024-12-04 10:22:19 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-red-600 text-2xl font-bold'>Vite + React</h1>
+      
+     
+     
+    </>
+  )
+}
+
+export default App
